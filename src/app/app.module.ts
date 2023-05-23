862bc5c1a197bab58840400d28d9a1bcd37bdeae
KorenSpell <kss00015@mix.wvu.edu>
2023-05-22 20:27:09 -0400

    Completed lab 2 with 2 new card components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideNavComponent } from './navigation/side-nav/side-nav.component';
+import { ExpectationsComponent } from './Components/expectations/expectations.component';
+import { ImportantComponent } from './Components/important/important.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideNavComponent
+    SideNavComponent,
+    ExpectationsComponent,
+    ImportantComponent
   ],
   imports: [
     BrowserModule,
